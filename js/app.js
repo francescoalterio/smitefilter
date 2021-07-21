8efd102f79214326c016882e2e02925243f3f76a
Francesco <francescoalteriog@gmail.com>
2021-07-21 15:25:19 -0400

    rrr

---
+++ js/app.js
@@ -0,0 +1,90 @@
+import { database } from './db/db.js';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    cargarDioses(database)
+})
+
+listener()
+function listener() {
+    const btnFiltrar = document.getElementById('btn-filtrar')
+
+    btnFiltrar.addEventListener('click', () => {
+        getValues()
+    })
+}
+
+const cargarDioses = gods => {
+    const main = document.querySelector('main')
+
+    gods.forEach( e => {
+        const box = document.createElement('div');
+        box.classList.add('box-dios')
+        box.setAttribute('style', `background-image: url("https://webcdn.hirezstudios.com/smite/god-cards/${e.url}.jpg")`);
+        box.innerHTML = `
+        <div class="box-datos-dios">
+            <div class="box-name-dios">
+                <p class="name-dios">${e.name.toUpperCase()}</p>
+            </div>
+            <div class="box-rolpan-dios">
+                <img src="../assets/roles/${e.rol}.jpg" alt="" class="dato-dios">
+                <img src="../assets/panteones/${e.panteon}.jpg" alt="" class="dato-dios">
+            </div>
+        </div>`
+
+        main.appendChild(box)
+        console.log('creado');
+    })
+}
+
+const getValues = () => {
+    const rol = document.getElementById('rol').value
+    const panteon = document.getElementById('panteon').value
+    const letra = document.getElementById('letra').value
+
+    filter(rol, panteon, letra)
+}
+
+const filter = (rol, panteon, letra) => {
+    const db = [...database]
+
+    if(rol || panteon || letra) {
+        const dbFiltered = db.filter( e => {
+            console.log(rol, panteon, letra);
+            if(rol && panteon && letra) {
+                return (e.rol == rol && e.panteon == panteon && e.name.charAt(0) == letra)
+            } if(rol && panteon) {
+                return (e.rol == rol && e.panteon == panteon)
+            } if(rol && letra) {
+                return (e.rol == rol && e.name.charAt(0) == letra)
+            } if(panteon && letra) {
+                return ( e.panteon == panteon && e.name.charAt(0) == letra )
+            } if(rol) {
+                return (e.rol == rol)
+            } if(panteon) {
+                return ( e.panteon == panteon ) 
+            } if(letra) {
+                return ( e.name.charAt(0) == letra )
+            }
+        
+        }) 
+        console.log(dbFiltered);
+
+        vaciarMain()
+        cargarDioses(dbFiltered)
+    } else {
+        vaciarMain()
+        cargarDioses(database)
+    }
+
+}
+
+const vaciarMain = () => {
+    const main = document.querySelector('main')
+
+    main.innerHTML = ''
+}
+
+
+
+
---
+++ js/db/db.js
@@ -0,0 +1,807 @@
+export const database = [
+    {
+        name:'afrodita',
+        url: 'aphrodite',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'agni',
+        url: 'agni',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'indu'
+    },
+    {
+        name:'ah muzen cab',
+        url: 'ah-muzen-cab',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'maya'
+    },
+    {
+        name:'aquiles',
+        url: 'achilles',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'griego'
+    },
+    {
+        name:'ah puch',
+        url: 'ah-puch',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'maya'
+    },
+    {
+        name:'amaterasu',
+        url: 'amaterasu',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'japones'
+    },
+    {
+        name:'anhur',
+        url: 'anhur',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'anubis',
+        url: 'anubis',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'ao kuang',
+        url: 'ao-kuang',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'chino'
+    },
+    {
+        name:'apolo',
+        url: 'apollo',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'griego'
+    },
+    {
+        name:'aracne',
+        url: 'arachne',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'griego'
+    },
+    {
+        name:'ares',
+        url: 'ares',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'artemisa',
+        url: 'artemis',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'griego'
+    },
+    {
+        name:'artio',
+        url: 'artio',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'celta'
+    },
+    {
+        name:'atenea',
+        url: 'athena',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'awilix',
+        url: 'awilix',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'maya'
+    },
+    {
+        name:'baba yaga',
+        url: 'baba-yaga',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'eslavo'
+    },
+    {
+        name:'baco',
+        url: 'bacchus',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'romano'
+    },
+    {
+        name:'bakasura',
+        url: 'bakasura',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'indu'
+    },
+    {
+        name:'baron samedi',
+        url: 'baron-samedi',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'vudu'
+    },
+    {
+        name:'bastet',
+        url: 'bastet',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'belona',
+        url: 'bellona',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'romano'
+    },
+    {
+        name:'cabrakan',
+        url: 'cabrakan',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'maya'
+    },
+    {
+        name:'camazotz',
+        url: 'camazotz',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'maya'
+    },
+    {
+        name:'cerbero',
+        url: 'cerberus',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'cernunnos',
+        url: 'cernunnos',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'celta'
+    },
+    {
+        name:'chaac',
+        url: 'chaac',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'maya'
+    },
+    {
+        name:'change',
+        url: 'change',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'chino'
+    },
+    {
+        name:'chernobog',
+        url: 'chernobog',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'eslavo'
+    },
+    {
+        name:'quiron',
+        url: 'chiron',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'griego'
+    },
+    {
+        name:'cronos',
+        url: 'chronos',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'cthulhu',
+        url: 'cthulhu',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'grandes antiguos'
+    },
+    {
+        name:'cuchulainn',
+        url: 'cu-chulainn',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'celta'
+    },
+    {
+        name:'cupido',
+        url: 'cupid',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'romano'
+    },
+    {
+        name:'da ji',
+        url: 'da-ji',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'chino'
+    },
+    {
+        name:'danzaburou',
+        url: 'danzaburou',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'japones'
+    },
+    {
+        name:'discordia',
+        url: 'discordia',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'romano'
+    },
+    {
+        name:'erlang shen',
+        url: 'erlang-shen',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'chino'
+    },
+    {
+        name:'isis',
+        url: 'eset',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'fafnir',
+        url: 'fafnir',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'nordico'
+    },
+    {
+        name:'fenrir',
+        url: 'fenrir',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'nordico'
+    },
+    {
+        name:'freya',
+        url: 'freya',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'nordico'
+    },
+    {
+        name:'ganesha',
+        url: 'ganesha',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'indu'
+    },
+    {
+        name:'geb',
+        url: 'geb',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'gilgamesh',
+        url: 'gilgamesh',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'babilonico'
+    },
+    {
+        name:'guan yu',
+        url: 'guan-yu',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'chino'
+    },
+    {
+        name:'hachiman',
+        url: 'hachiman',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'japones'
+    },
+    {
+        name:'hades',
+        url: 'hades',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'he bo',
+        url: 'he-bo',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'chino'
+    },
+    {
+        name:'heimdal',
+        url: 'heimdallr',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'nordico'
+    },
+    {
+        name:'hel',
+        url: 'hel',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'nordico'
+    },
+    {
+        name:'hera',
+        url: 'hera',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'hercules',
+        url: 'hercules',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'romano'
+    },
+    {
+        name:'horus',
+        url: 'horus',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'hou yi',
+        url: 'hou-yi',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'chino'
+    },
+    {
+        name:'hun batz',
+        url: 'hun-batz',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'maya'
+    },
+    {
+        name:'izanami',
+        url: 'izanami',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'japones'
+    },
+    {
+        name:'jano',
+        url: 'janus',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'romano'
+    },
+    {
+        name:'jing wei',
+        url: 'jing-wei',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'chino'
+    },
+    {
+        name:'jormungandr',
+        url: 'jormungandr',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'nordico'
+    },
+    {
+        name:'kali',
+        url: 'kali',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'indu'
+    },
+    {
+        name:'khepri',
+        url: 'khepri',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'rey arturo',
+        url: 'king-arthur',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'arturiano'
+    },
+    {
+        name:'kukulkan',
+        url: 'kukulkan',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'maya'
+    },
+    {
+        name:'kumbhakarna',
+        url: 'kumbhakarna',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'indu'
+    },
+    {
+        name:'kuzenbo',
+        url: 'kuzenbo',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'japones'
+    },
+    {
+        name:'loki',
+        url: 'loki',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'nordico'
+    },
+    {
+        name:'medusa',
+        url: 'medusa',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'griego'
+    },
+    {
+        name:'mercurio',
+        url: 'mercury',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'romano'
+    },
+    {
+        name:'merlin',
+        url: 'merlin',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'arturiano'
+    },
+    {
+        name:'morgana le fay',
+        url: 'morgan-le-fay',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'arturiano'
+    },
+    {
+        name:'mulan',
+        url: 'mulan',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'chino'
+    },
+    {
+        name:'ne zha',
+        url: 'ne-zha',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'chino'
+    },
+    {
+        name:'neith',
+        url: 'neith',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'nemesis',
+        url: 'nemesis',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'griego'
+    },
+    {
+        name:'nike',
+        url: 'nike',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'griego'
+    },
+    {
+        name:'nox',
+        url: 'nox',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'romano'
+    },
+    {
+        name:'nu wa',
+        url: 'nu-wa',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'chino'
+    },
+    {
+        name:'odin',
+        url: 'odin',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'nordico'
+    },
+    {
+        name:'olorun',
+        url: 'olorun',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'yoruba'
+    },
+    {
+        name:'osiris',
+        url: 'osiris',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'pele',
+        url: 'pele',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'polinesio'
+    },
+    {
+        name:'persefone',
+        url: 'persephone',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'poseidon',
+        url: 'poseidon',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'ra',
+        url: 'ra',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'raijin',
+        url: 'raijin',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'japones'
+    },
+    {
+        name:'rama',
+        url: 'rama',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'indu'
+    },
+    {
+        name:'ratatoskr',
+        url: 'ratatoskr',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'nordico'
+    },
+    {
+        name:'ravana',
+        url: 'ravana',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'indu'
+    },
+    {
+        name:'scylla',
+        url: 'scylla',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'serket',
+        url: 'serqet',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'set',
+        url: 'set',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'skadi',
+        url: 'skadi',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'nordico'
+    },
+    {
+        name:'sobek',
+        url: 'sobek',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'sol',
+        url: 'sol',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'nordico'
+    },
+    {
+        name:'sun wukong',
+        url: 'sun-wukong',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'chino'
+    },
+    {
+        name:'susano',
+        url: 'susano',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'japones'
+    },
+    {
+        name:'silvano',
+        url: 'sylvanus',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'romano'
+    },
+    {
+        name:'terra',
+        url: 'terra',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'romano'
+    },
+    {
+        name:'tanatos',
+        url: 'thanatos',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'griego'
+    },
+    {
+        name:'morrigan',
+        url: 'the-morrigan',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'celta'
+    },
+    {
+        name:'thor',
+        url: 'thor',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'nordico'
+    },
+    {
+        name:'thoth',
+        url: 'thoth',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'egipcio'
+    },
+    {
+        name:'tiamat',
+        url: 'tiamat',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'babilonico'
+    },
+    {
+        name:'tsukuyomi',
+        url: 'tsukuyomi',
+        rol: 'assassin',
+        damage: 'fisico',
+        panteon: 'japones'
+    },
+    {
+        name:'tyr',
+        url: 'tyr',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'nordico'
+    },
+    {
+        name:'ullr',
+        url: 'ullr',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'nordico'
+    },
+    {
+        name:'vamana',
+        url: 'vamana',
+        rol: 'warrior',
+        damage: 'fisico',
+        panteon: 'indu'
+    },
+    {
+        name:'vulcano',
+        url: 'vulcan',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'romano'
+    },
+    {
+        name:'ixbalanque',
+        url: 'xbalanque',
+        rol: 'adc',
+        damage: 'fisico',
+        panteon: 'maya'
+    },
+    {
+        name:'xing tian',
+        url: 'xing-tian',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'chino'
+    },
+    {
+        name:'yemaya',
+        url: 'yemoja',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'yoruba'
+    },
+    {
+        name:'ymir',
+        url: 'ymir',
+        rol: 'support',
+        damage: 'magico',
+        panteon: 'nordico'
+    },
+    {
+        name:'zeus',
+        url: 'zeus',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'griego'
+    },
+    {
+        name:'zhong kui',
+        url: 'zhong-kui',
+        rol: 'mage',
+        damage: 'magico',
+        panteon: 'chino'
+    },
+]
