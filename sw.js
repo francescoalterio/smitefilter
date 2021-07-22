b7495d46914aa84780cd47d629fad41e9db66102
Francesco <francescoalteriog@gmail.com>
2021-07-22 19:54:30 -0400

    erwe

--- sw.js
+++ sw.js
@@ -1,6 +1,24 @@
+const nombreCache = 'apv-1';
+const archivos = [
+    '/',
+    '/index.html',
+    '/css/normalize.css',
+    '/css/style.css',
+    '/js/db/db.js',
+    '/js/app.js',
+    '/js/pwa.js'
+]
 
 self.addEventListener('install', e => {
-    console.log('Instalado el serviceWorker')
+    console.log('Instalado el serviceWorker');
+    
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then( cache => {
+                console.log('Cacheando');
+                cache.addAll(archivos)
+            })
+    )
 })
 
 self.addEventListener('activate', e => {
@@ -10,4 +28,4 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
     console.log('Fetch...', e);
-}
+})
