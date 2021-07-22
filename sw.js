4f132ba6e79f6fb545db27ba80ef9ba5281a2655
Francesco <francescoalteriog@gmail.com>
2021-07-22 19:15:58 -0400

    cargando datos de cache

--- sw.js
+++ sw.js
@@ -28,4 +28,9 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
     console.log('Fetch...', e);
+
+    e.respondWith(
+        caches.match(e.request)
+            .then( respuestaCache => respuestaCache
+    )
 })
